aeae07c9ac1845ec0eee73389a4f06de0912680b
Kenta Yoshida <yoshida.kenta@gene.com>
2019-02-15 14:06:09 -0800

    Add test with stan model fit

--- tests/testthat/test-stan_emax.R
+++ tests/testthat/test-stan_emax.R
@@ -1,13 +1,57 @@
 context("test-stan_emax.R")
 
-data <- dplyr::tibble(a = c(4,6,7),
-                      b = c(8,11,15),
-                      c = c(23,45,76))
+library(dplyr)
+
+set.seed(0)
+
+## Generate data
+
+nsample.dose   <- 10
+clearance      <- 0.5
+logsd.exposure <- 0.7
+
+dose.exposure <-
+  tibble(dose = rep(c(0, 100, 1000), each = nsample.dose)) %>%
+  mutate(exposure = dose / clearance * exp(rnorm(dose, 0, logsd.exposure)))
+
+e0   <- 10
+emax <- 100
+ec50 <- 100
+sd.response <- 10
+
+test.data <-
+  dose.exposure %>%
+  mutate(response =
+           e0 + emax * exposure / (ec50 + exposure) +
+           rnorm(exposure, 0, sd.response))
+
+test.standata <-
+  with(test.data,
+  list(exposure = exposure,
+       response = response,
+       N = length(response)))
+
 
 test_that("check formula elements", {
-  expect_equal(2 * 2, 4)
-  expect_error(stan_emax(cbind(b, c) ~ a, data),
+  expect_error(stan_emax(cbind(dose, response) ~ exposure, test.data),
                "Only one response variable is allowed")
-  expect_error(stan_emax(b ~ a + c, data),
+  expect_error(stan_emax(response ~ dose + exposure, test.data),
                "Only one exposure variable is allowed")
 })
+
+
+set.seed(123)
+
+test.fit <- stan_emax_run(stanmodels$mod_emax_e0,
+                          test.standata,
+                          chains = 2, iter = 500,
+                          refresh = 0)
+
+test_that("emax model run", {
+  expect_is(test.fit, "stanemax")
+  expect_equal(dim(test.fit$stanfit), c(250, 2, 66))
+  expect_equal(rstan::summary(test.fit$stanfit, pars = c("emax"))$summary[[1]],
+               expected = 100, tolerance = 0.05, scale = 100)
+})
+
+
