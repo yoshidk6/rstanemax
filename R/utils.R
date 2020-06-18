9500039714c4a4c81b4eab8aa92a4dc0f9312260
Kenta Yoshida <6.kurabupasu@gmail.com>
2020-06-18 07:56:11 -0700

    Update Rd to clear CRAN check

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,4 @@
-#' Pipe operator
-#'
-#' See \code{\link[magrittr]{\%>\%}} for more details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
 #' @importFrom magrittr %>%
 #' @export
-#' @usage lhs \%>\% rhs
-NULL
-
-
+magrittr::`%>%`
 
