d6594fd17573887abb965a3791daa2c4aed7114e
Kenta Yoshida <yoshida.kenta.6@gmail.com>
2024-11-26 05:38:16 -0800

    add test for pars_wo_index

--- tests/testthat/test-as_draws.R
+++ tests/testthat/test-as_draws.R
@@ -3,26 +3,29 @@ set.seed(123)
 
 mods <- list()
 pars <- list()
+pars_wo_index <- list()
 
 mods[[1]] <- suppressWarnings(stan_emax(
   formula = response ~ exposure,
   data = exposure.response.sample,
-  chains = 2,
-  iter = 500,
+  chains = 1, iter = 50, refresh = 0,
   show_messages = FALSE
 ))
 pars[[1]] <- c("ec50", "sigma", "gamma", "e0", "emax")
+pars_wo_index[[1]] <- c("ec50", "sigma", "gamma", "e0", "emax")
 
 mods[[2]] <- suppressWarnings(stan_emax(
   formula = resp ~ conc,
   data = exposure.response.sample.with.cov,
   param.cov = list(emax = "cov1", e0 = "cov2"),
+  chains = 1, iter = 50, refresh = 0,
   show_messages = FALSE
 ))
 pars[[2]] <- c(
   "ec50", "sigma", "gamma", "e0[B0]", "e0[B2]",
   "e0[B3]", "emax[A0]", "emax[A1]"
 )
+pars_wo_index[[2]] <- c("ec50", "sigma", "gamma", "e0", "emax")
 
 
 test_that("as_draws coercion works", {
@@ -79,6 +82,14 @@ test_that("as_draws preserves covariate labels", {
         with_indices = TRUE
       ),
       expected = pars[[i]]
+    )
+
+    expect_equal(
+      object = posterior::variables(
+        x = posterior::as_draws_rvars(mods[[i]]),
+        with_indices = FALSE
+      ),
+      expected = pars_wo_index[[i]]
     )
   }
 })
